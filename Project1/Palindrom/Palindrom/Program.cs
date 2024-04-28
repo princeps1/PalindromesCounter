ec2fba389d0fb66602da22b66a230f2a2e2b3223
princeps1 <matejajovic2002@gmail.com>
2024-04-28 17:13:06 +0200

    Added structure and function for listening

---
+++ Project1/Palindrom/Palindrom/Program.cs
@@ -0,0 +1,21 @@
+﻿using Palindrom.Services;
+using Palindrom.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileUtil.StartListening($"http://localhost:5050/");
+            //Counter.GetNumberOfPalindromes();
+        }
+    }
+}
---
+++ Project1/Palindrom/Palindrom/Utils/FileUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom.Utils
+{
+    public class FileUtil
+    {
+        static HttpListener listener = new HttpListener();
+
+        public static void StartListening(string prefix)//u slucaju da funkcija nema parametara
+                                                        //string? prefix=null
+        {
+            listener.Prefixes.Add(prefix);
+            listener.Start();
+        }
+        public static string GetFile();
+
+
+
+    }
+}
